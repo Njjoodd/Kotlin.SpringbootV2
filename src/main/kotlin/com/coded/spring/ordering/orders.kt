e740c699543f86b384fff6a1d3d0f8751bb81a29
Njood <nejood03@gmail.com>
2025-04-17 17:46:03 +0300

    Task 2 : Online ordering - post an order

---
+++ src/main/kotlin/com/coded/spring/ordering/orders.kt
@@ -0,0 +1,23 @@
+package com.coded.spring.ordering
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/orders")
+class OrderController {
+
+    val orderDatabase = mutableListOf<Order>()
+
+    @PostMapping
+    fun placeOrder (@RequestBody order: Order): String { orderDatabase.add(order)
+        return "Order received!"
+    }
+
+    @GetMapping
+    fun listOrders(): List<Order> {
+        return orderDatabase
+    }
+}
+
+data class Order(val user: String, val resturant: String, val items: List<String>)
+
